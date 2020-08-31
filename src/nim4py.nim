2c86fa1952c0fe71f87d037facbcf0a2c4a53dc9
Juan Carlos <JuanCarlospaco@gmail.com>
2020-08-31 15:51:38 -0300

    :O

--- src/nim4py.nim
+++ src/nim4py.nim
@@ -1,13 +1,29 @@
-import compiler/nimeval, nimpy
+import os, compiler/nimeval, nimpy
 
-proc nimscript(script: string; stdlib: openArray[string]; verbose: bool = false) {.exportpy.} =
+
+let folder = currentSourcePath().splitPath.head
+let stdlib = @[
+  folder / "lib",
+  folder / "lib" / "arch",
+  folder / "lib" / "core",
+  folder / "lib" / "deprecated",
+  folder / "lib" / "experimental",
+  folder / "lib" / "impure",
+  folder / "lib" / "packages",
+  folder / "lib" / "posix",
+  folder / "lib" / "pure",
+  folder / "lib" / "std",
+  folder / "lib" / "system",
+  folder / "lib" / "windows",
+  folder / "lib" / "wrappers",
+]
+
+
+proc nimscript(script: string; stdlib: seq[string] = stdlib) {.exportpy.} =
   ## NimScript Interpreter for Python, see https://nim-lang.github.io/Nim/nims.html
   ## func nimscript(script: string; stdlib: openArray[string]; verbose: bool = false)
   assert script.len > 0, "NimScript must not be empty string"
   assert stdlib.len > 0, "stdlib must not be empty openArray"
-  if unlikely(verbose): echo "NimScript: createInterpreter()"
   let interpreter = createInterpreter(script, stdlib)
-  if unlikely(verbose): echo "NimScript: evalScript()"
   interpreter.evalScript()
-  if unlikely(verbose): echo "NimScript: destroyInterpreter()"
   interpreter.destroyInterpreter()
