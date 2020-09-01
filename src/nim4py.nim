361de993dd5937d1c414bd67c2da960c53886aa0
Juan Carlos <JuanCarlospaco@gmail.com>
2020-08-31 22:49:33 -0300

    Almost there

--- src/nim4py.nim
+++ src/nim4py.nim
@@ -1,25 +1,7 @@
-import os, compiler/nimeval, nimpy
+import compiler/nimeval, nimpy
 
 
-let folder = currentSourcePath().splitPath.head
-let stdlib = @[
-  folder / "lib",
-  folder / "lib" / "arch",
-  folder / "lib" / "core",
-  folder / "lib" / "deprecated",
-  folder / "lib" / "experimental",
-  folder / "lib" / "impure",
-  folder / "lib" / "packages",
-  folder / "lib" / "posix",
-  folder / "lib" / "pure",
-  folder / "lib" / "std",
-  folder / "lib" / "system",
-  folder / "lib" / "windows",
-  folder / "lib" / "wrappers",
-]
-
-
-proc nimscript(script: string; stdlib: seq[string] = stdlib) {.exportpy.} =
+proc nimscript(script: string; stdlib: seq[string]) {.exportpy.} =
   ## NimScript Interpreter for Python, see https://nim-lang.github.io/Nim/nims.html
   ## func nimscript(script: string; stdlib: openArray[string]; verbose: bool = false)
   assert script.len > 0, "NimScript must not be empty string"
